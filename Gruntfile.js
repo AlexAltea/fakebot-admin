2bfdf5b91536d7eb82acf149d1460610e261c5a1
Alexandro Sanchez Bach <s8alsanc@stud.uni-saarland.de>
2017-04-11 00:19:32 +0200

    Initial site

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,50 @@ module.exports = function (grunt) {
 
     // Project configuration
     grunt.initConfig({
+        path: {
+            app: 'app',
+            dist: 'dist'
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    '<%= path.dist %>/fakebot-admin.css': [
+                        '<%= path.app %>/styles/**/*.css'
+                    ]
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= path.dist %>',
+                    src: '{,*/}*.html',
+                    dest: '<%= path.dist %>/'
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    '<%= path.dist %>/fakebot-admin.js': [
+                        '<%= path.app %>/scripts/*.js'
+                    ]
+                }
+            }
+        },
         connect: {
             options: {
-                port: 9001,
-                livereload: 35729,
-                hostname: 'localhost'
+                port: 9000,
+                livereload: 35728,
+                hostname: 'localhost',
             },
             livereload: {
                 options: {
@@ -22,11 +61,19 @@ module.exports = function (grunt) {
             }
         },
         watch: {
+            scripts: {
+                files: '<%= path.app %>/scripts/{,*/}*.js',
+                tasks: ['uglify']
+            },
+            styles: {
+                files: '<%= path.app %>/styles/{,*/}*.css',
+                tasks: ['cssmin']
+            },
             livereload: {
                 files: [
                     '*.html',
-                    '*.css',
-                    '*.js',
+                    '<%= path.dist %>/*.js',
+                    '<%= path.dist %>/*.css'
                 ],
                 options: {
                     livereload: '<%= connect.options.livereload %>'
@@ -37,6 +84,9 @@ module.exports = function (grunt) {
 
     // Project tasks
     grunt.registerTask('build', [
+        'cssmin',
+        'htmlmin',
+        'uglify',
     ]);
     grunt.registerTask('serve', [
         'connect',
